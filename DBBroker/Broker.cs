f38f9a29a176ad2498cf25a3cad769cab0a1086b
bokycodes <114671954+bokycodes@users.noreply.github.com>
2024-01-29 19:45:37 +0100

    Dodao Broker baze podataka i povezao se na bazu

---
+++ DBBroker/Broker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBBroker
+{
+    public class Broker
+    {
+        private SqlConnection connection;
+
+        public Broker()
+        {
+            connection = new SqlConnection(@"Data Source=DESKTOP-C1E55NG\SQLEXPRESS;Initial Catalog=BoxingClubDB;Integrated Security=True;");
+        }
+
+        public void OpenConnection()
+        {
+            connection?.Open();
+        }
+
+        public void CloseConnection()
+        {
+            connection?.Close();
+        }
+
+        //kod koji komunicira sa bazom ovde pises (metode)
+    }
+}
