0ad8461d61bfaad519e16a64e5e967d8ecac4cf9
bokycodes <114671954+bokycodes@users.noreply.github.com>
2024-03-13 13:40:06 +0100

    Napravio klasu za server koja ce da radi sa osluskivanjem socketa itd

---
+++ Server/Server.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class Server
+    {
+        private Socket socket;
+
+        public Server()
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void Start()
+        {
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9999);
+            socket.Bind(endpoint);
+            socket.Listen(5);
+        }
+
+        public void Listen()
+        {
+
+        }
+
+        public void Stop()
+        {
+            socket.Close();
+        }
+    }
+}
