58ae1fc59234bb7f8d54fac34708a6d65729baf0
bokicodes <114671954+bokicodes@users.noreply.github.com>
2024-05-16 13:48:08 +0200

    Dodao potpis metode za ucitavanje liste objekata

--- Zajednicko/domen/IDomenskiObjekat.cs
+++ Zajednicko/domen/IDomenskiObjekat.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +11,7 @@ namespace Zajednicko.domen
     {
         string NazivTabele { get; }
         string VrednostiZaUneti { get; }
+
+        IDomenskiObjekat KreirajObjekat(SqlDataReader reader);
     }
 }
