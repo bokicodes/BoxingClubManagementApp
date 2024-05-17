8fc88c743054cba4dcb00541fb0f526b2642cd28
bokicodes <114671954+bokicodes@users.noreply.github.com>
2024-05-17 10:28:54 +0200

    Implementirao genericku metodu za ucitavanje liste i pozvao je u operaciji

--- Zajednicko/domen/Korisnik.cs
+++ Zajednicko/domen/Korisnik.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,10 +8,24 @@ using System.Threading.Tasks;
 namespace Zajednicko.domen
 {
     [Serializable]
-    public class Korisnik
+    public class Korisnik : IDomenskiObjekat
     {
         public int KorisnikId { get; set; }
         public string KorisnickoIme { get; set; }
         public string Lozinka { get; set; }
+
+        public string NazivTabele => "Korisnik";
+
+        public string VrednostiZaUneti => throw new NotImplementedException();
+
+        public IDomenskiObjekat KreirajObjekat(SqlDataReader reader)
+        {
+            Korisnik k = new Korisnik
+            {
+                KorisnickoIme = (string)reader["KorisnickoIme"],
+                Lozinka = (string)reader["Lozinka"]
+            };
+            return k;
+        }
     }
 }
