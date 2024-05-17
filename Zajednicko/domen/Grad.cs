c1b3cb1cf6aab2b66e61e20ff4cd4050dfe214de
bokicodes <114671954+bokicodes@users.noreply.github.com>
2024-05-17 10:28:36 +0200

    Implementirao genericku metodu za ucitavanje liste i pozvao je u operaciji

--- Zajednicko/domen/Grad.cs
+++ Zajednicko/domen/Grad.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,11 +8,26 @@ using System.Threading.Tasks;
 namespace Zajednicko.domen
 {
     [Serializable]
-    public class Grad
+    public class Grad : IDomenskiObjekat
     {
         public int GradId { get; set; }
         public string Naziv { get; set; }
         public string PostanskiBroj { get; set; }
+
+        public string NazivTabele => "Grad";
+
+        public string VrednostiZaUneti => throw new NotImplementedException();
+
+        public IDomenskiObjekat KreirajObjekat(SqlDataReader reader)
+        {
+            Grad grad = new Grad()
+            {
+                GradId = (int)reader["GradId"],
+                Naziv = (string)reader["Naziv"],
+                PostanskiBroj = (string)reader["PostanskiBroj"]
+            };
+            return grad;
+        }
 
         public override string ToString()
         {
